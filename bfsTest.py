e919593dee7f52e0eeb2269761cc6a6be046b510
Sada <qudcks3000@gmail.com>
2021-06-13 18:34:42 +0900

    bfs-concept

---
+++ bfsTest.py
@@ -0,0 +1,31 @@
+def Bfs(graph, start):
+    visit = []
+    queue = []
+
+    queue.append(start)
+
+    while queue:
+        node = queue.pop(0)
+        if node not in visit:
+            visit.append(node)
+            queue.extend(graph[node])
+    return(visit)
+
+
+graph = {
+    'A': ['B'],
+    'B': ['A', 'C', 'H'],
+    'C': ['B', 'D'],
+    'D': ['C', 'E', 'G'],
+    'E': ['D', 'F'],
+    'F': ['E'],
+    'G': ['D'],
+    'H': ['B', 'I', 'J', 'M'],
+    'I': ['H'],
+    'J': ['H', 'K'],
+    'K': ['J', 'L'],
+    'L': ['K'],
+    'M': ['H']
+}
+
+print(Bfs(graph, 'D'))
