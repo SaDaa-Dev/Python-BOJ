2650c4cff7ccc7c64c0b238c98282bfc5eaf8aca
noteBook <qudcks3000@gmail.com>
2021-10-27 16:48:01 +0900

    1157-solve

---
+++ 1157.py
@@ -0,0 +1,16 @@
+# 1157 단어 공부
+import sys
+target = sys.stdin.readline().rstrip().upper()
+re = []
+reStr = []
+while True:
+    re.append(target.count(target[0]))
+    reStr.append(target[0])
+    target = target.replace(target[0], "")
+    if len(target) < 1:
+        break
+
+if re.count(max(re)) > 1:
+    print("?")
+else:
+    print(reStr[re.index(max(re))])
